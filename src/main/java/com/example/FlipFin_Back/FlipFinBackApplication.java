e8402358a6adad70e3ad612eaaf783da7eeb020d
Switch1511 <gustavopasqualli0@gmail.com>
2025-02-16 21:53:41 -0300

    First commit

---
+++ src/main/java/com/example/FlipFin_Back/FlipFinBackApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlipFin_Back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlipFinBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlipFinBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/FlipFin_Back/FlipFinBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlipFin_Back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlipFinBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

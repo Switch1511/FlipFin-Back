8164a973f25be9dd44197947c706eaf708b46017
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-09 23:05:23 -0300

    feat: Add annotation model

--- src/main/java/com/example/FlipFin/model/Annotation.java
+++ src/main/java/com/example/FlipFin/model/Annotation.java
@@ -1,2 +1,43 @@
-package com.example.FlipFin.model;public class Annotation {
+package com.example.FlipFin.model;
+
+import jakarta.persistence.*;
+
+@Entity(name = "annotation")
+@Table(name = "annotation")
+public class Annotation {
+
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String title;
+    private String description;
+
+    public Annotation() {
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
 }

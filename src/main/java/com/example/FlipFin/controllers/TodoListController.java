27a6f0325b990bb4817c3a4811f289b68964a877
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-09 23:04:33 -0300

    feat: Add put in todolist controller

--- src/main/java/com/example/FlipFin/controllers/TodoListController.java
+++ src/main/java/com/example/FlipFin/controllers/TodoListController.java
@@ -1,12 +1,14 @@
 package com.example.FlipFin.controllers;
 
 
+import com.example.FlipFin.controllers.dto.taskDescription;
 import com.example.FlipFin.domain.Task;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RestController
@@ -29,6 +31,19 @@ public class TodoListController {
     public ResponseEntity<Void> deleteTask(@PathVariable Long taskId) {
         tasks.removeIf(task -> task.id().equals(taskId));
 
+        return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping
+    public ResponseEntity<Void> updateTask(@PathVariable Long taskId,
+                                           @RequestBody taskDescription dto){
+        tasks = tasks.stream()
+                .map(task -> {
+                    if (task.id().equals(taskId)) {
+                        return new Task(task.id(), dto.description());
+                    }
+                    return task;
+                }).collect(Collectors.toCollection(ArrayList::new));
         return ResponseEntity.noContent().build();
     }
 }

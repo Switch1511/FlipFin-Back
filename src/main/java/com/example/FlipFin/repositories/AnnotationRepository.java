d7dd60f4f9308ea1815867bad518ee2a7fa130fb
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-09 23:05:40 -0300

    feat: Add annotation repository

--- src/main/java/com/example/FlipFin/repositories/AnnotationRepository.java
+++ src/main/java/com/example/FlipFin/repositories/AnnotationRepository.java
@@ -1,2 +1,10 @@
-package com.example.FlipFin.repositories;public class AnnotationRepository {
+package com.example.FlipFin.repositories;
+
+
+import com.example.FlipFin.model.Annotation;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AnnotationRepository extends JpaRepository<Annotation, Integer> {
 }

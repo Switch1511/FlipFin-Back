edc3163a7f8cfc95e6a3083981d550f7f5551cc1
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-10 00:15:18 -0300

    feat: Add getbyid delete and put annotation

--- src/main/java/com/example/FlipFin/controllers/AnnotationController.java
+++ src/main/java/com/example/FlipFin/controllers/AnnotationController.java
@@ -3,6 +3,7 @@ package com.example.FlipFin.controllers;
 import com.example.FlipFin.controllers.dto.AnnotationDto;
 import com.example.FlipFin.model.Annotation;
 import com.example.FlipFin.repositories.AnnotationRepository;
+import org.apache.coyote.Response;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/annotation")
@@ -31,5 +33,35 @@ public class AnnotationController {
         var annotation = new Annotation();
         BeanUtils.copyProperties(dto, annotation);
         return ResponseEntity.status(HttpStatus.CREATED).body(repository.save(annotation));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity getById(@PathVariable(value = "id") Integer id) {
+        Optional<Annotation> annotation = repository.findById(id);
+        if(annotation.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Annotation not found");
+        }
+        return ResponseEntity.status(HttpStatus.FOUND).body(annotation.get());
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity delete(@PathVariable(value = "id") Integer id) {
+        Optional<Annotation> annotation = repository.findById(id);
+        if(annotation.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Annotation not found");
+        }
+        repository.delete(annotation.get());
+        return ResponseEntity.status(HttpStatus.OK).body("Annotation deleted");
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity update(@PathVariable(value = "id") Integer id, @RequestBody AnnotationDto dto) {
+        Optional<Annotation> annotation = repository.findById(id);
+        if(annotation.isEmpty()) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Annotation not found");
+        }
+        var annotationModel = annotation.get();
+        BeanUtils.copyProperties(dto, annotationModel);
+        return ResponseEntity.status(HttpStatus.OK).body(repository.save(annotationModel));
     }
 }

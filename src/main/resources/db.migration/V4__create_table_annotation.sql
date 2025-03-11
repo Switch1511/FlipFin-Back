5c95535e60bd864c8dd7d93d0fa674a4f9dff912
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-10 23:19:18 -0300

    fix: Ajust in annotation migration

--- src/main/resources/db.migration/V4__create_table_annotation.sql
+++ src/main/resources/db.migration/V4__create_table_annotation.sql
@@ -2,7 +2,5 @@ CREATE TABLE annotation (
     id SERIAL PRIMARY KEY,
     title VARCHAR(255) NOT NULL,
     description VARCHAR(255) NOT NULL,
-    usuario_id BIGINT NOT NULL,
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-     FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );

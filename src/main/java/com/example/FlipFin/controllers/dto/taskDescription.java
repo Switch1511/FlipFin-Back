925006b926ef29bcc13a4c1561bc4492ddb038a5
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-10 23:18:13 -0300

    feat: Add todolist dto

--- src/main/java/com/example/FlipFin/controllers/dto/taskDescription.java
+++
@@ -1,4 +0,0 @@
-package com.example.FlipFin.controllers.dto;
-
-public record taskDescription(String description) {
-}
--- src/main/java/com/example/FlipFin/domain/Annotation.java
+++
@@ -1,4 +0,0 @@
-package com.example.FlipFin.domain;
-
-public record Annotation(Long id, String title, String description) {
-}

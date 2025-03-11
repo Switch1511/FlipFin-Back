401fb906919340973fde17c2a46e752b5d94bcb0
Switch1511 <gustavopasqualli0@gmail.com>
2025-03-10 23:19:10 -0300

    fix: Ajust in todolist migration

--- src/main/resources/db.migration/V3__create_table_todolist.sql
+++ src/main/resources/db.migration/V3__create_table_todolist.sql
@@ -1,9 +1,7 @@
 CREATE TABLE todolist (
     id BIGSERIAL PRIMARY KEY,
-    titulo VARCHAR(255) NOT NULL,
-    descricao TEXT,
-    dificuldade VARCHAR(10) NOT NULL CHECK (dificuldade IN ('facil', 'medio', 'dificil')),
-    data_criacao TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-    usuario_id BIGINT NOT NULL,
-    FOREIGN KEY (usuario_id) REFERENCES usuarios(id) ON DELETE CASCADE
+    title VARCHAR(255) NOT NULL,
+    description TEXT,
+    difficulty VARCHAR(10) NOT NULL CHECK (difficulty IN ('easy', 'medium', 'hard')),
+    created_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
